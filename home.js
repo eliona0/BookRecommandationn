86cd516891971e522bc3dcafffc4590c696548d5
AnisaKajtazi <kajtazianisa1@gmail.com>
2025-01-24 16:03:24 +0100

    Login and Signin Update

--- home.js
+++ home.js
@@ -1,27 +1,27 @@
-function openLogin(){
-    const loginBox = document.getElementById('login-box');
-    const signupBox = document.getElementById('signup-box');
-    if(loginBox.style.display==='none'){
-        loginBox.style.display='block';
-        signupBox.style.display='none';
-    }
-    else{
-        loginBox.style.display='none';
-    }
-}
+// function openLogin(){
+//     const loginBox = document.getElementById('login-box');
+//     const signupBox = document.getElementById('signup-box');
+//     if(loginBox.style.display==='none'){
+//         loginBox.style.display='block';
+//         signupBox.style.display='none';
+//     }
+//     else{
+//         loginBox.style.display='none';
+//     }
+// }
 
 
-function openSignUp(){
-    const loginBox = document.getElementById('login-box');
-    const signupBox = document.getElementById('signup-box');
-    if( signupBox.style.display === 'none'){
-        signupBox.style.display='block';
-        loginBox.style.display='none';
-    }
-    else{
-        signupBox.style.display='none';
-    }
-}
+// function openSignUp(){
+//     const loginBox = document.getElementById('login-box');
+//     const signupBox = document.getElementById('signup-box');
+//     if( signupBox.style.display === 'none'){
+//         signupBox.style.display='block';
+//         loginBox.style.display='none';
+//     }
+//     else{
+//         signupBox.style.display='none';
+//     }
+// }
 
 // script.js
 document.querySelector('.hamburger-menu').addEventListener('click', function () {
